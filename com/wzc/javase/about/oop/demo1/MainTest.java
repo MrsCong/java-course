dd9e8c7a1513b1fb5e1a14cb578fb1a7a081d4cb
wuzhicong <1251516543@qq.com>
2021-12-12 12:02:02 +0800

    第一次测试提交

---
+++ com/wzc/javase/about/oop/demo1/MainTest.java
@@ -0,0 +1,7 @@
+package com.wzc.javase.about.oop.demo1;
+
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ out/production/java-course/README.md
@@ -0,0 +1,5 @@
+# Introduction to Java
+
+This your copy of the code for the java-course. The course itself is located in the issues of this repository.
+
+第一次提交
