2784c9a44d8a9e7a2e76022a0242a6c04a4b7964
wuzhicong <1251516543@qq.com>
2022-02-21 20:52:29 +0800

    2022-22-15 gc  Test

---
+++ com/wzc/javase/about/oop/demo1/GcTest.java
@@ -0,0 +1,9 @@
+package com.wzc.javase.about.oop.demo1;
+
+public class GcTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- com/wzc/javase/about/oop/demo1/MainTest.java
+++ com/wzc/javase/about/oop/demo1/MainTest.java
@@ -2,6 +2,18 @@ package com.wzc.javase.about.oop.demo1;
 
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person person = new Person();
+        for (int i = 0;i<1000;i++) {
+            person = null;
+        }
+        System.gc();
+    }
+
+    static class Person {
+        @Override
+        protected void finalize() throws Throwable {
+            System.out.println("我该回收了~");
+            super.finalize();
+        }
     }
 }
